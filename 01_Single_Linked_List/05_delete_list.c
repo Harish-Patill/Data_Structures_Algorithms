4c7ca68ee4ff4b5fe16d7b17cdf86408ac044c90
Harish-Patill <hbpatil275@gmail.com>
2026-08-04 22:29:28 +0530

    delete first

--- 01_Single_Linked_List/05_delete_list.c
+++ 01_Single_Linked_List/05_delete_list.c
@@ -1,5 +1,17 @@
 #include "sll.h"
 
 int sl_delete_list(S_list **head){
-	
-}
+	S_list *temp=*head;                             // a temporary variable that holds the current address of where head is pointing to
+
+    if(*head == NULL){                              // if list is empty, end
+        return FAILURE;
+    }
+
+    while(temp != NULL){                            // let this run until we hit the last node
+        *head=(*head)->link;                        // update the head with the next nodes address
+        free(temp);                                 // free the current node(1st node as of now as it is pointing to the first node)
+        temp=*head;                                 // now assign temp with the next nodes address;
+
+    }
+    return SUCCESS;
+}   
