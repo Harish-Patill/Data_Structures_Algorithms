9d589a5a5ebd81207b1b3fe363ef8d6e95e2ef63
Harish-Patill <hbpatil275@gmail.com>
2026-08-05 23:07:51 +0530

    sll: insert before

--- 01_Single_Linked_List/08_insert_before.c
+++ 01_Single_Linked_List/08_insert_before.c
@@ -1,6 +1,37 @@
 #include "sll.h"
 
-int sl_insert_before(Slist **head, data_t g_data, data_t ndata)
-{
+int sl_insert_before(S_list **head, int given_data, int new_data){
+    S_list *temp=*head;
+    S_list *prev=NULL;
 
+    if(*head == NULL){                          // list is empty
+        return LIST_EMPTY;
+    }
+
+    while(temp != NULL){
+        if(temp->data == given_data){                       // if element found
+            S_list *new_node=malloc(sizeof(S_list));        // create a node
+
+            if(new_node == NULL){                           // check if node is created or not
+                return FAILURE;
+            }
+
+            new_node->data=new_data;     
+            
+            if(prev == NULL){                               // if the node if first
+                new_node->link=*head;
+                *head=new_node;
+            }
+            else{                                           // will be executed when the node is not first
+                new_node->link=temp;
+                prev->link=new_node;
+            }
+            
+            return SUCCESS;
+        }
+
+        prev=temp;
+        temp=temp->link;
+    }
+    return DATA_NOT_FOUND;
 }
