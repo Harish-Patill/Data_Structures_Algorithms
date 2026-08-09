5128b70be4d3942934d163332e144bd22fc7cdd0
Harish-Patill <hbpatil275@gmail.com>
2026-08-09 21:00:24 +0530

    sll : delete element

--- 01_Single_Linked_List/10_delete_element.c
+++ 01_Single_Linked_List/10_delete_element.c
@@ -2,4 +2,29 @@
 
 int sl_delete_element(S_list **head, int data){
 
+    if(*head==NULL){                                // if empty list, end it
+        return FAILURE;
+    }
+    
+    S_list *temp=*head;
+    
+    if(temp->data==data){                           // check if its the first node
+        *head=temp->link;
+        free(temp);
+        return SUCCESS;
+    }
+
+    S_list *prev=temp;
+    temp = temp->link;
+    
+    while(temp!=NULL){                              
+        if(temp->data==data){
+            prev->link=temp->link;
+            free(temp);
+         return SUCCESS;
+        }
+        prev=temp;
+        temp=temp->link;
+    }
+   return DATA_NOT_FOUND;
 }
