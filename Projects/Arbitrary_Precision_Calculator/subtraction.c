05db783e273b6f7f30b939b1180587c5609ee800
Harish-Patill <hbpatil275@gmail.com>
2026-08-13 13:29:02 +0530

    done

--- Projects/Arbitrary_Precision_Calculator/subtraction.c
+++ Projects/Arbitrary_Precision_Calculator/subtraction.c
@@ -65,11 +65,20 @@ int subtraction(node *head1, node *head2, node *tail1, node *tail2, node **resul
         }
 
         // drain the remaining digits of operand 1 to the ans list
-        while(tail1 != NULL){                                                                       // once there are remaining elements in the 1st list
+        while(tail1 != NULL){     
+            // once there are remaining elements in the 1st list
+            
+            if(tail1->data < 0){                          // a borrow from an earlier step left this digit negative
+                tail1->data = tail1->data + 10;
+                if(tail1->prev != NULL){
+                    tail1->prev->data = tail1->prev->data - 1;
+                }
+            }        
             ans=tail1->data;                                                                // just go on adding them to the list with the count edge case
             insert_at_first(result_head,result_tail,ans);
             tail1=tail1->prev;
         }
+        return POSITIVE;
     }
 
     // case 3: if the 2nd operand is long
@@ -92,10 +101,16 @@ int subtraction(node *head1, node *head2, node *tail1, node *tail2, node **resul
 
         // drain the remaining digits of operand 2, to the ans list
         while(tail2 != NULL){                                                                       // once there are remaining elements in the 1st list
+            if(tail2->data < 0){                                                            // a borrow from an earlier step left this digit negative
+                tail2->data = tail2->data + 10;
+                if(tail2->prev != NULL){
+                    tail2->prev->data = tail2->prev->data - 1;
+                }
+            }
             ans=tail2->data;                                                                // just go on adding them to the list with the count edge case
             insert_at_first(result_head,result_tail,ans);
             tail2=tail2->prev;
         }
+        return NEGATIVE;
     }
-    
 }
